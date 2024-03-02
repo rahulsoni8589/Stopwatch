fd2697b92dd7998d2cf9e883889c16d0b9146f44
rahulsoni8589 <rajsoni9644@outlook.com>
2024-03-02 16:08:44 +0530

    Created stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,50 @@
+let minutes = 0;
+let seconds = 0;
+let interval;
+
+const timerElement = document.querySelector(".timer");
+const startElement = document.querySelector("#startBtn");
+const stopElement = document.querySelector("#stopBtn");
+const resetElement = document.querySelector("#resetBtn");
+
+timerElement.textContent = `${"0" + minutes} : ${"0" + seconds}`
+
+
+startElement.addEventListener("click", startwatch)
+
+resetElement.addEventListener("click", resetwatch)
+
+stopElement.addEventListener("click", stopwatch)
+
+
+function startwatch(){
+  interval = setInterval(()=>{
+    seconds++
+    if (seconds === 60){
+      minutes++;
+      seconds = 0;
+      }
+    
+    seconds = seconds<10? "0"+seconds:seconds;
+    minutes = minutes<10? "0"+minutes:minutes;
+
+    timerElement.innerHTML = `${minutes} : ${seconds}`
+    }, 1000)
+    startElement.disabled = true 
+}
+
+function resetwatch(){
+  minutes = 0;
+  seconds = 0;
+  clearInterval(interval);
+  timerElement.textContent = `${"0" + minutes} : ${"0" + seconds}`;
+  startElement.disabled = false
+}
+   
+function stopwatch(){
+  clearInterval(interval);
+  startElement.disabled = false
+  
+}
+
+
